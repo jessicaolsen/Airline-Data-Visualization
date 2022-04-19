f3602d94bf62501b5c77c62df4ee6558a2e0ebec
Jessica Olsen <jaolsen@yahoo.com>
2022-04-19 15:55:00 -0400

    New Dashboard for looking at Airline Performance

--- airline_visualization.py
+++ airline_visualization.py
@@ -8,7 +8,7 @@ from dash import dcc
 #Read the airline data into Panda Dataframe
 airline_data = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv',
                             encoding = "ISO-8859-1", 
-                            dtype = {'DivlAirport': str, 'DivlTailNum' : str, 'Div2Airport': str, 'Div2TailNum': str})
+                            dtype = {'Div1Airport': str, 'Div1TailNum' : str, 'Div2Airport': str, 'Div2TailNum': str})
 
 #Randomly sample 500 data points
 data = airline_data.sample(n= 500, random_state = 42)
